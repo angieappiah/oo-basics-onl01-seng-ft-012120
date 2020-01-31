881c47e8aff3956f73daeccaf8f0ed747f690811
Angela Appiah <alegnalawn2@yahoo.com>
2020-01-31 07:18:02 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -16,6 +16,18 @@ class Book
     @author
   end
   
+  def page_count=(num)
+    @page_count= num 
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+  def genre= (genre)
+    @genre= genre
+  end
+  
   
 end
 
